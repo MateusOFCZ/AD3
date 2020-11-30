0123fbd5dd477613439a585a16ec4dbc130a8ac8
MateusOFCZ <mateusdacruz357@hotmail.com>
2020-11-30 19:34:09 -0300

    Imports e Packages

---
+++ AD3/src/CONTROLLER/AD3.java
@@ -0,0 +1,12 @@
+package CONTROLLER;
+
+import VIEW.Login;
+public class AD3 {
+
+    public static void main(String[] args) {
+
+        Login telaInicio = new Login();
+        telaInicio.setVisible(true);        
+    }
+
+}
