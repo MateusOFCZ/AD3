1841ae478ea785c515122731f6ce47acd8ff1e39
MarcoJr <marcoanbjr@gmail.com>
2020-11-30 19:21:57 -0300

    Upload do Projeto

---
+++ AD3/src/ad3/AD3.java
@@ -0,0 +1,16 @@
+package ad3;
+
+import ad3.View.Login;
+
+/**
+ * @author MarcoJr
+ */
+public class AD3 {
+
+    public static void main(String[] args) {
+
+        Login telaInicio = new Login();
+        telaInicio.setVisible(true);        
+    }
+
+}
